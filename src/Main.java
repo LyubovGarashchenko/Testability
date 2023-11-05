d3338c7d1fea5fe18dbc128f2b97079f56dea020
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2023-11-05 17:52:22 +0300

    Chenge cod java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int myPrice = service.calculate(10_000);
-        int price = 10_000;
-        price = service.calculate(price); // должно получиться 500
-        System.out.println(price);
+        System.out.println(myPrice);
     }
 }
