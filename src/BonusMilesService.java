a481fcb191433311c87d9208dc367e4892037f88
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2023-11-08 12:16:11 +0300

    The last

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,7 @@
 public class BonusMilesService {
-    public int calculate(int price) {
-        int amount = 10_000; // стоимость билета
-        int bonus = 20; // за каждые 20 рублей начисляется 1 миля
-        price = amount / bonus;
+    public int calculate(int amount, int bonus) {
+        bonus = 20; // за каждые 20 рублей начисляется 1 миля
+        int price = amount / bonus;
         return price;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int myPrice = service.calculate(10_000);
+        int myPrice = service.calculate(10_000, 20);
         System.out.println(myPrice);
     }
 }
