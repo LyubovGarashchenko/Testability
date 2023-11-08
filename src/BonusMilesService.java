ee2725e1156a8d1f6ddbf18bb8fde6a13acaa7af
Lyubov Garashchenko <lyubov-balabina@yandex.ru>
2023-11-08 12:17:53 +0300

    The last commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,5 @@
 public class BonusMilesService {
     public int calculate(int amount, int bonus) {
-        bonus = 20; // за каждые 20 рублей начисляется 1 миля
         int price = amount / bonus;
         return price;
     }
